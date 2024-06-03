7eba4b573eaf2aad64a366ccb0c09ff2967fdc29
Ken <kennedymindeman@gmail.com>
2024-06-03 00:08:38 -0500

    Change indentation to spaces
    Godot uses tabs by default this can be changed in Editor > Text Editor >
    Behavior > Type

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -13,42 +13,42 @@ var started = false
 var process_input = true
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
+    $AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
-	if jump_pressed():
-		if not started:
-			start()
+    if jump_pressed():
+        if not started:
+            start()
 
-		jump()
+        jump()
 
-	if not started:
-		return
+    if not started:
+        return
 
-	velocity.y += GRAVITY * delta
-	velocity.y = min(velocity.y, MAX_SPEED)
-	move_and_collide(velocity * delta)
-	tilt()
+    velocity.y += GRAVITY * delta
+    velocity.y = min(velocity.y, MAX_SPEED)
+    move_and_collide(velocity * delta)
+    tilt()
 
 func jump():
-	velocity.y = FLAP_FORCE
-	rotation_degrees = -30
+    velocity.y = FLAP_FORCE
+    rotation_degrees = -30
 
 func tilt():
-	if velocity.y > 0:
-		rotation_degrees = min(90, rotation_degrees + ROTATION_SPEED)
+    if velocity.y > 0:
+        rotation_degrees = min(90, rotation_degrees + ROTATION_SPEED)
 
 func done():
-	process_input = false
+    process_input = false
 
 func stop():
-	$AnimatedSprite2D.stop()
-	velocity = Vector2.ZERO
+    $AnimatedSprite2D.stop()
+    velocity = Vector2.ZERO
 
 func start():
-	$AnimatedSprite2D.play("fly")
-	game_started.emit()
-	started = true
+    $AnimatedSprite2D.play("fly")
+    game_started.emit()
+    started = true
 
 func jump_pressed():
-	return Input.is_action_just_pressed("ui_up")
+    return Input.is_action_just_pressed("ui_up")
